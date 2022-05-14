526091cef02784c706b3897adec65aac3d91cce5
panutat-p <pp.panutat@gmail.com>
2022-05-14 22:50:56 +0700

    Nuxt pages

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Head>
+    <Title>Nuxt App</Title>
+    <Meta name="description" content="lorem ipsum"/>
+  </Head>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Hello there</h1>
+        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+          quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</template>
