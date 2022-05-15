f03a75a9e4cd6bb13bb0cd65fbffafd25d8ba0e4
panutat-p <pp.panutat@gmail.com>
2022-05-15 21:32:30 +0700

    custom PageNotFound, with slots (Failed to use layout)

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <div class="container mx-auto text-center">
+      <div class="bg-stone-200">
+        <slot name="header"></slot>
+      </div>
+      <div class="bg-blue-200">
+        <slot name="detail" class="bg-blue-200"></slot>
+      </div>
+    </div>
+  </main>
+</template>
--- pages/404.vue
+++ pages/404.vue
@@ -1,30 +1,48 @@
+<script setup lang="ts">
+/*
+Nuxt bugs ?
+NuxtLayout should be sufficient to do the job
+ */
+definePageMeta({
+  layout: false,
+  title: 'not found',
+})
+</script>
+
 <template>
   <main>
-    <div class="container">
-      <h1 class="text-5xl text-red-400 m-10">Page Not Found</h1>
-    </div>
-    <div class="carousel carousel-center rounded-box">
-      <div class="carousel-item">
-        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=8B7BCDC2" alt="Pizza" />
-      </div>
-      <div class="carousel-item">
-        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=500B67FB" alt="Pizza" />
-      </div>
-      <div class="carousel-item">
-        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=A89D0DE6" alt="Pizza" />
-      </div>
-      <div class="carousel-item">
-        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=225E6693" alt="Pizza" />
-      </div>
-      <div class="carousel-item">
-        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=9D9539E7" alt="Pizza" />
-      </div>
-      <div class="carousel-item">
-        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=BDC01094" alt="Pizza" />
-      </div>
-      <div class="carousel-item">
-        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=7F5AE56A" alt="Pizza" />
-      </div>
-    </div>
+    <NuxtLayout name="error">
+      <template #header>
+        <h1 class="text-5xl my-8">Page Not Found</h1>
+        <div class="carousel carousel-center rounded-box">
+          <div class="carousel-item">
+            <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=8B7BCDC2" alt="Pizza"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=500B67FB" alt="Pizza"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=A89D0DE6" alt="Pizza"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=225E6693" alt="Pizza"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=9D9539E7" alt="Pizza"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=BDC01094" alt="Pizza"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=7F5AE56A" alt="Pizza"/>
+          </div>
+        </div>
+      </template>
+      <template #detail>
+        <div class="my-8 text-3xl text-blue-600">
+          <NuxtLink to="/" class="link">Go Back</NuxtLink>
+        </div>
+      </template>
+    </NuxtLayout>
   </main>
 </template>
