5e959c41376ae4fc221015aca9aa8ecc52e9d41b
panutat-p <pp.panutat@gmail.com>
2022-05-15 20:53:20 +0700

    custom PageNotFound

---
+++ pages/404.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <div class="container">
+      <h1 class="text-5xl text-red-400 m-10">Page Not Found</h1>
+    </div>
+    <div class="carousel carousel-center rounded-box">
+      <div class="carousel-item">
+        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=8B7BCDC2" alt="Pizza" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=500B67FB" alt="Pizza" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=A89D0DE6" alt="Pizza" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=225E6693" alt="Pizza" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=9D9539E7" alt="Pizza" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=BDC01094" alt="Pizza" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=7F5AE56A" alt="Pizza" />
+      </div>
+    </div>
+  </main>
+</template>
