f98c6b02d8f0f461fee247cba0d40bf8402c4fa1
panutat-p <pp.panutat@gmail.com>
2022-05-22 22:49:39 +0700

    Member page, GetProfile

--- composables/use-auth.ts
+++ composables/use-auth.ts
@@ -1,3 +1,5 @@
+import {useStorage} from '@vueuse/nuxt/node_modules/@vueuse/core';
+
 export async function useLogIn(email, password) {
     const {data: r, error: e} = await useLazyFetch(
         'https://api.codingthailand.com/api/login',
@@ -10,5 +12,22 @@ export async function useLogIn(email, password) {
             initialCache: false,
         }
     );
-    return { r, e };
+    return {r, e};
+}
+
+export async function useIsLogIn() {
+    const token = useStorage('nuxtToken', null);
+    return token.value !== null;
+}
+
+export async function useGetProfile() {
+    const token = JSON.parse(useStorage('nuxtToken', null).value);
+    const {data: r} = await useLazyFetch(
+        'https://api.codingthailand.com/api/profile',
+        {
+            method: 'GET',
+            headers: {Authorization: `Bearer ${token?.access_token}`},
+        }
+    );
+    return {r};
 }
--- pages/member.vue
+++ pages/member.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const {r} = await useGetProfile();
+</script>
 
 <template>
-  <div>
-    <p class="text-6xl">
-      Member Only
-    </p>
+  <div class="hero min-h-screen" style="background-image: url(https://api.lorem.space/image/fashion?w=1000&h=800);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl">HELLO!</h1>
+        <h1 class="mb-5 text-3xl">id: {{ r?.data?.user?.id }}</h1>
+        <h1 class="mb-5 text-3xl">name: {{ r?.data?.user?.name }}</h1>
+        <h1 class="mb-5 text-3xl">Email: {{ r?.data?.user?.email }}</h1>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
   </div>
 </template>
